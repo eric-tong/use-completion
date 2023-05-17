6586e1af269ab7f942e1493b4cb5afdc5ad3a4e9
Eric Tong <eric.yintong@gmail.com>
2023-05-17 17:14:43 +0000

    [Docs] Add JSDocs

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,10 @@
+/**
+ *
+ * @summary Return type for useCompletion Hook
+ * @param {string} prompt Text that is sent to the model for completion
+ * @param {string} apiKey OpenAI API Key
+ * @param {string} [model] Name of generative model to be used
+ */
 export type Completion = {
     text: string | undefined,
     isFetching: boolean,
--- src/useCompletion.ts
+++ src/useCompletion.ts
@@ -3,6 +3,14 @@ import { Configuration, OpenAIApi } from 'openai'
 import type { Completion } from './types';
 import { DEFAULT_MODEL } from './constants';
 
+/**
+ *
+ * @summary Creates a completion for the provided prompt
+ * @param {string} prompt Text that is sent to the model for completion
+ * @param {string} apiKey OpenAI API Key {@link https://platform.openai.com/account/api-keys} 
+ *    WARNING: Be careful not to leak your API key by using this in public-facing apps
+ * @param {string} [model] Name of generative model to be used. Defaults to {@link DEFAULT_MODEL}
+ */
 export default function useCompletion(prompt: string, apiKey: string, model: string = DEFAULT_MODEL): Completion {
     const [text, setText] = useState<string>()
     const [isFetching, setIsFetching] = useState<boolean>(true)
