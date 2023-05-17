b3799b4180eb97139b2c88e73da65aecf50be385
Eric Tong <eric.yintong@gmail.com>
2023-05-17 17:27:34 +0000

    [Hook] Refactor to use useCompletionWithConfig

--- src/useCompletion.ts
+++ src/useCompletion.ts
@@ -1,31 +1,19 @@
-import { useState } from 'react';
-import { Configuration, OpenAIApi } from 'openai'
+import { Configuration } from 'openai'
 import type { Completion } from './types';
 import { DEFAULT_MODEL } from './constants';
+import useCompletionWithConfig from './useCompletionWithConfig';
 
 /**
  *
- * @summary Creates a completion for the provided prompt
+ * @summary Simple hook to create a completion for the provided prompt. Use {@link useCompletionWithConfig} to configure the query in detail
  * @param {string} prompt Text that is sent to the model for completion
  * @param {string} apiKey OpenAI API Key {@link https://platform.openai.com/account/api-keys} 
  *    WARNING: Be careful not to leak your API key by using this in public-facing apps
  * @param {string} [model] Name of generative model to be used. Defaults to {@link DEFAULT_MODEL}
  */
 export default function useCompletion(prompt: string, apiKey: string, model: string = DEFAULT_MODEL): Completion {
-    const [text, setText] = useState<string>()
-    const [isFetching, setIsFetching] = useState<boolean>(true)
-    const [error, setError] = useState<Error>()
-
     const configuration = new Configuration({ apiKey })
     const request = { prompt, model }
-    const openai = new OpenAIApi(configuration)
 
-    openai.createCompletion(request)
-        .then(response => response.data)
-        .then(data => data.choices[0]?.text!!)
-        .then(text => setText(text))
-        .then(() => setIsFetching(false))
-        .catch(setError)
-
-    return { text, isFetching, error }
+    return useCompletionWithConfig(request, configuration)
 }
---
+++ src/useCompletionWithConfig.ts
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import { OpenAIApi } from 'openai';
+import type { Configuration, CreateCompletionRequest } from 'openai'
+import type { Completion } from './types';
+
+/**
+ *
+ * @summary Hook to create a completion for the provided prompt with configuration options
+ * @param {CreateCompletionRequest} request Configuration for the request to be sent to the Completion API, including the prompt and model name
+ * @param {Configuration} configuration Configuration for OpenAI API, including API key
+ */
+export default function useCompletion(request: CreateCompletionRequest, configuration: Configuration): Completion {
+    const [text, setText] = useState<string>()
+    const [isFetching, setIsFetching] = useState<boolean>(true)
+    const [error, setError] = useState<Error>()
+
+    const openai = new OpenAIApi(configuration)
+    openai.createCompletion(request)
+        .then(response => response.data)
+        .then(data => data.choices[0]?.text!!)
+        .then(text => setText(text))
+        .then(() => setIsFetching(false))
+        .catch(setError)
+
+    return { text, isFetching, error }
+}
